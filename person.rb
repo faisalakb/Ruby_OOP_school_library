4ed3ecccb865a1e89274047c7a404fdc5831f323
faisalakb <faisalakb786@gmail.com>
2023-10-18 19:23:02 +0500

    update

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
 # Represents a person's personal information.
 class Person
-  attr_reader :id, :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
   def initialize(id, age, name: 'Unknown', parent_permission: true)
     @id = id
@@ -12,10 +13,6 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
-
-  attr_writer :name
-
-  attr_writer :age
 
   private
 
